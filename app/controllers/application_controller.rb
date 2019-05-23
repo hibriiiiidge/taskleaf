54d470f4418c95f01417c3463733c2f52d03de68
Yasunori Takahashi <hibriiiiidge@gmail.com>
2019-05-23 12:45:48 +0900

    reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,2 @@
 class ApplicationController < ActionController::Base
-  #before_action :set_basic_auth
-  before_action :set_basic_auth, if: :is_production?
-
-  private
-
-  def is_production?
-    Rails.env.production?
-  end
-
-  # def set_basic_auth
-  #   authenticate_or_request_with_http_basic do |name, password|
-  #     name == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #   end
-  # end
-  def set_basic_auth
-    authenticate_or_request_with_http_basic do |name, password|
-      name == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
 end
